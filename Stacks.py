ab27c59f1b0866c8d5ca261f86e22a6afe2582cf
Gabriel Xavier <100116691+0nlySpirit@users.noreply.github.com>
2024-06-30 21:28:47 -0300

    Code to learn about Stacks

---
+++ Stacks.py
@@ -0,0 +1,29 @@
+#By Gabriel Xavier - Dotket :]
+#Learning Python - Backend
+
+# ============ Practice Stacks ============
+
+class Stacks:
+    def __init__(self):
+        self.items = []
+        
+    def __length__(self):
+        return len(self.items)
+    
+    def __push__(self, data):
+        self.items.append(data)
+        
+    def __pop__(self):
+        if self.__length__() == 0:
+            return None
+        else:
+            self.items.pop()
+            
+    def __display__(self):
+        it = self.items
+        
+        for i in it:
+            print(i)
+            
+# :] - 'Test Here' -----------------------------------------------------------------------    
+            
