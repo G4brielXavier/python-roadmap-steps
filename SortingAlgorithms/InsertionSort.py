57ef1a45d9998f7614ea7e511a5d8f2d11c73354
Gabriel Xavier <100116691+0nlySpirit@users.noreply.github.com>
2024-07-16 00:39:17 -0300

    Learning about Algorithms of Sorting

---
+++ SortingAlgorithms/InsertionSort.py
@@ -0,0 +1,21 @@
+#By Gabriel Xavier - Dotket :]
+#Learning Python - Backend
+
+# ============ Insertion Sort ============
+
+numbers = [5, 4, 6, 7, 8, 1, 2, 3]
+
+def insertionSort(arr):
+    for i in range(1, len(arr)):
+        a = arr[i]
+        j = i - 1
+        
+        while j >= 0 and a < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        
+        arr[j + 1] = a
+        
+    return arr
+
+ 
