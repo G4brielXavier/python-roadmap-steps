590fa4b8211de95795640db967024bac10af1e02
Gabriel Xavier <100116691+0nlySpirit@users.noreply.github.com>
2024-07-20 02:42:02 -0300

    Learning about decorators in functions

--- Decorators/about.py
+++ Decorators/about.py
@@ -8,16 +8,20 @@
 # Call function into of other function and validate
 
 # high order function 
-def validate(func):
-    def valid(x, y):
-        if x < 0 or y < 0:
-            raise ValueError('ERROR')
-        return func(x, y)
-    
+
+def validate_username(func):
+    def valid(name):
+        length_name = len(name)
+        if not length_name > 4:
+            print('Length username not accepted')
+            return
+        print('Accepted username')
+        return func(name)
     return valid
 
-@validate
-def sum(n1, n2):
-    return n1 + n2
+@validate_username
+def return_username(name):
+    return name
 
-print(sum(10, 10))
+edit_profile_name = input('Username: ')
+profile_name = return_username(edit_profile_name)
