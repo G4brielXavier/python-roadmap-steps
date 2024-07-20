9ea2b271f8ea0f0aa4968ef9d001ad7a2838b798
Gabriel Xavier <100116691+0nlySpirit@users.noreply.github.com>
2024-07-19 21:30:28 -0300

    Learning about Sorting Algorithms

--- SortingAlgorithms/InsertionSort.py
+++ SortingAlgorithms/InsertionSort.py
@@ -9,13 +9,15 @@ def insertionSort(arr):
     for i in range(1, len(arr)):
         a = arr[i]
         j = i - 1
-        
+    
         while j >= 0 and a < arr[j]:
             arr[j + 1] = arr[j]
             j -= 1
         
         arr[j + 1] = a
-        
+    
     return arr
 
+insertionSort(numbers)
+print(numbers)
  
