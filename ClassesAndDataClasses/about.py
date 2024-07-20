84fef3211d0ad23d70ee8e5d50bdb637aa5917df
Gabriel Xavier <100116691+0nlySpirit@users.noreply.github.com>
2024-07-20 02:41:34 -0300

    Learning about Classes and Data classes

--- ClassesAndDataClasses/about.py
+++ ClassesAndDataClasses/about.py
@@ -3,40 +3,27 @@
 
 # ============ Classes and DataClasses ============
 
-#Creating a class normal of models cars
-print('Normal Class')
-class Info_Model:
-    def __init__(self, brand, model, mph):
-        self.brand = brand
-        self.model = model
-        self.mph = mph
-        
-    def __show__(self):
-        print(f'Brand: {self.brand}')
-        print(f'Model: {self.model}')
-        print(f'Mph: {self.mph} Mph')
-    
-car_one = Info_Model('Chevrolet', 'Onix LTZ', 130)
-car_one.__show__()
-print()
+#Creating a class normal to sign-up persons
 
-#Creating a class with DataClasses Library of models cars
+class People:
+    def __init__(self, name, sobrenome, sexo, data_nascimento):
+        self.name = name
+        self.sobrenome = sobrenome
+        self.sexo = sexo
+        self.data_nascimento = data_nascimento
+    
+    
+cliente_1 = People('Gabriel', 'Xavier', 'M', '2006-05-29')
+
+#Creating a class with DataClasses Library of sign-up persons
 
 from dataclasses import dataclass
 
-print('Using DataClasses')
 @dataclass
-class Info_Model_New:
-    brand: str
-    model: str
-    mph: int
-    engine: str
+class People_class:
+    name: str
+    sobrenome: str
+    sexo: str
+    data_nascimento: str
     
-    def __show__(self):
-        print(f'Brand: {self.brand}')
-        print(f'Model: {self.model}')
-        print(f'Mph: {self.mph} Mph')
-        print(f'Engine: {self.engine}')
-    
-car_one = Info_Model_New('Hyundai', 'Creta', 160, 'LXC-09')
-car_one.__show__()
+cliente_plus = People_class('Dot', 'Ket', 'M', '2006/05/29')
