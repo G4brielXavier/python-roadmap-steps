b42ab57ec17abc1815a38d0dda0affe8f787cdc3
Gabriel Xavier <100116691+0nlySpirit@users.noreply.github.com>
2024-07-19 21:29:59 -0300

    Learning about BubbleSort

--- SortingAlgorithms/BubbleSort.py
+++ SortingAlgorithms/BubbleSort.py
@@ -7,16 +7,13 @@ numbers = [2, 1, 7, 6, 8, 3, 4, 5, 6, 9]
 
 def bubbleSort(arr):
     end_index = len(arr)
-    
+
     while end_index > 0:
-        j = 0
-        while j < end_index - 1:
-            if arr[j] > arr[j + 1]:
-                temp = arr[j]
-                arr[j] = arr[j + 1]
-                arr[j + 1] = temp
-            j += 1
-        end_index -= 1
+        i = 0
+        while i < end_index - 1:
+            if arr[i] > arr[i+1]:
+                temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
         
-bubbleSort(numbers)
-print(numbers)
+        end_index -= 1
