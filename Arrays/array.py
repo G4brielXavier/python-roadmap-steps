2068c6dcaf16dc095eedc125e2d5b4dbbfde7645
Gabriel Xavier <100116691+0nlySpirit@users.noreply.github.com>
2024-07-31 15:28:56 -0300

    Learning about arrays

--- Arrays/array.py
+++ Arrays/array.py
@@ -3,8 +3,15 @@
 
 # ============ Practice Arrays ============
 
+# pip install numpy
 import numpy as np
 
 # One Dimension: Vector
 # Two Dimension: Matrix
-# Three Dimensio: Tensor
+# Three Dimension: Tensor
+
+#create a vector
+vector = np.array([1, 2, 3])
+
+# create a matrix
+matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
