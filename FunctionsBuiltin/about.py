d79498ea070c86fbbc6a020f5013a93765836839
Gabriel Xavier <100116691+0nlySpirit@users.noreply.github.com>
2024-07-31 15:30:44 -0300

    Learning abou Built-in functions

---
+++ FunctionsBuiltin/about.py
@@ -0,0 +1,24 @@
+#By Gabriel Xavier - Dotket :]
+#Learning Python - Backend
+
+# ============ Functions and Built-in Functions ============
+
+# Functions
+
+def good_morning():
+    print('Good Morning')
+    
+def sum(n1, n2):
+    return n1 + n2
+
+
+# Built-in Functions
+
+bin()
+hex()
+oct()
+input()
+sum()
+max()
+min()
+pow()
