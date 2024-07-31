8ed912169de8b2c85b3cdfe141e40c043ac863a0
Gabriel Xavier <100116691+0nlySpirit@users.noreply.github.com>
2024-07-31 15:31:21 -0300

    Learning about Linked List

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -11,7 +11,7 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
-        
+    
     def __append__(self, data):
         new_node = Node(data)
         
@@ -19,42 +19,83 @@ class LinkedList:
             self.head = new_node
             return
         
-        current_node = self.head 
+        current_node = self.head
         
         while current_node.next:
             current_node = current_node.next
             
         current_node.next = new_node
-        return
-    
+        
     def __len__(self):
         if self.head is None:
             return 0
         
-        tot = 0
+        len_number = 0
         current_node = self.head
         
         while current_node:
-            tot += 1
-            current_node = current_node.next 
+            len_number += 1
+            current_node = current_node.next
             
-        return tot
+        return len_number
     
     def __to_list__(self):
         node_data = []
-        
         current_node = self.head
         
         while current_node:
             node_data.append(current_node.data)
             current_node = current_node.next
-        
-        return node_data    
+            
+        return node_data
     
-    def __show__(self):
+    def __get__(self, index):
+        if index >= self.__len__() or index < 0:
+            print('ERROR: Index out of range')
+            return
+        
+        current_index = 0
+        current_node = self.head
+        
+        while current_node is not None:
+            if current_index == index:
+                return current_node.data
+            current_node = current_node.next
+            current_index += 1
+            
+    def __invert__(self):
+        previous_node = None
+        current_node = self.head
+        
+        while current_node is not None:
+            next = current_node.next
+            current_node.next = previous_node
+            prev = current_node
+            current_node = next
+            
+        self.head = previous_node
+        
+    def __search__(self, data):
         if self.head is None:
-            print('The list has no element to show')
-            return None
+            print('List has no element')
+            return
+        
+        current_node = self.head
+        
+        while current_node is not None:
+            if current_node.data == data:
+                print('Founded')
+                return True
+            current_node = current_node.next
+            
+        print('Not founded')
+        return False
+      
+      
+    def __display__(self):
+        if self.head is None:
+            print('List has no elements to show')
+            return
         
         current_node = self.head
         
@@ -62,77 +103,35 @@ class LinkedList:
             print(current_node.data)
             current_node = current_node.next
             
-    def __invert__(self):
-        previous_node = None
-        current_node = self.head
+    def remove_at_start(self):
+        if self.head is None:
+            print('List has no element')
+            return
         
-        while current_node:
-            next = current_node.next
-            current_node.next = previous_node
-            previous_node = current_node
-            current_node = next
-            
-        self.head = previous_node
-
-    def __get__(self, index):
-        if index >= self.__len__() or index < 0:
-            print('ERROR: Index out of range')
-            
-        current_index = 0
-        current_node = self.head
+        self.head = self.head.next
         
-        while current_node != None:
-            if current_index == index:
-                return current_node.data
-            current_index += 1
-            current_node = current_node.next
-            
-    def __search__(self, data):
+    def remove_at_end(self):
         if self.head is None:
             print('List has no element')
             return
         
         current_node = self.head
-        while current_node != None:
-            if current_node.data == data:
-                print('Element founded')
-                return True
+
+        while current_node.next.next is not None:
             current_node = current_node.next
             
-        print('Element not found')
-        return False
-    
-    def __remove_at_start__(self):
-        if self.head is None:
-            print('The list has no element to remove')
-            return
-        
-        self.head = self.head.next
-    
-    def __remove_at_end__(self):
-        if self.head is None:
-            print('The list has no elements to remove')
-            return 
-        
+    def remove_element_by_data(self, data):
         current_node = self.head
         
-        while current_node.next.next != None:
-            current_node = current_node.next
-            
-        current_node.next = None
-
-    def __remove_element_by_value__(self, data):
-        current_node = self.head
-
-        if current_node != None:
+        if current_node is not None:
             if current_node.data == data:
                 self.head = current_node.next
                 current_node = None
                 return
             
-        while current_node != None:
+        while current_node is not None:
             if current_node.data == data:
-                break
+                break 
             prev = current_node
             current_node = current_node.next
             
@@ -141,13 +140,14 @@ class LinkedList:
         
         prev.next = current_node.next
         current_node = None
-
-    def __insert_at_start__(self, data):
+        
+    def insert_at_start(self, data):
         new_node = Node(data)
         new_node.next = self.head
         self.head = new_node
         
-    def __insert_at_end__(self, data):
+        
+    def insert_at_end(self, data):
         new_node = Node(data)
         
         if self.head is None:
@@ -156,40 +156,40 @@ class LinkedList:
         
         current_node = self.head
         
-        while current_node.next != None:
+        while current_node is not None:
             current_node = current_node.next
             
-        current_node.next = new_node
-
-    def __insert_at_index__(self, index, data):
+        current_node = new_node
+        
+        
+    def insert_at_index(self, index, data):
         if index == 1:
             new_node = Node(data)
             new_node.next = self.head
             self.head = new_node
             
-        id = 0
+        i = 1
         current_node = self.head
         
-        while id < index - 1 and current_node != None:
+        while i < index-1 and current_node is not None:
             current_node = current_node.next
-            id += 1
+            i += 1
             
-            if current_node is None:
-                print('ERROR: Index out of range')
-            else:
-                new_node = Node(data)
-                new_node.next = current_node.next
-                current_node.next = new_node
+        if current_node is None:
+            print('ERROR: Index out of range')
+        else:
+            new_node = Node(data)
+            self.head = current_node.next
+            current_node.next = new_node
+            
+
             
 # :] - 'Test Here' ------------------------------------------------------------------------------------------------------------
 
-mylist = LinkedList()
+myLL = LinkedList()
 
-for i in range(1, 10+1):
-    mylist.__append__(i)
-
-mylist.__insert_at_index__(4, 100)
-mylist.__show__()
-
-
-print(f'Length: {mylist.__len__()}')
+for i in range(10):
+    myLL.__append__(i)
+    
+print(f'Quem está no índice 5 é o valor: {myLL.__get__(5)}')
+myLL.__search__(100)
