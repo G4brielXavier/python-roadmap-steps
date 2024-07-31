5703e5b9ab2b5bcd885eb59c3a55dca793352443
Gabriel Xavier <100116691+0nlySpirit@users.noreply.github.com>
2024-07-31 15:31:43 -0300

    Learning about Stacks

--- Stacks/Stacks.py
+++ Stacks/Stacks.py
@@ -7,22 +7,17 @@ class Stacks:
     def __init__(self):
         self.items = []
         
-    def __length__(self):
+    def __len__(self):
         return len(self.items)
     
-    def __push__(self, data):
+    def __add__(self, data):
         self.items.append(data)
         
-    def __pop__(self):
-        if self.__length__() == 0:
-            return None
+    def __pop__(self, data):
+        if self.__len__() == 0:
+            return
         
         self.items.pop()
             
-    def __display__(self):
-        it = self.items
-        
-        for i in it:
-            print(i)
             
 # :] - 'Test Here' -----------------------------------------------------------------------      
