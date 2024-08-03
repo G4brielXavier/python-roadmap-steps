fbe512401b86a28ac0191f301d1f57978fb2917e
Gabriel Xavier <100116691+0nlySpirit@users.noreply.github.com>
2024-08-03 19:23:41 -0300

    Learning about Sorting Algorithms

--- SortingAlgorithms/BubbleSort.py
+++ SortingAlgorithms/BubbleSort.py
@@ -5,15 +5,22 @@
 
 numbers = [2, 1, 7, 6, 8, 3, 4, 5, 6, 9]
 
-def bubbleSort(arr):
+def BubbleSort(arr):
+    
     end_index = len(arr)
-
+    
     while end_index > 0:
-        i = 0
-        while i < end_index - 1:
-            if arr[i] > arr[i+1]:
-                temp = arr[i]
-                arr[i] = arr[i+1]
-                arr[i+1] = temp
         
+        i = 0
+        
+        while i < end_index - 1:
+            
+            if arr[i] > arr[i + 1]:
+                
+                temp = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = temp
+                
+            i += 1
+            
         end_index -= 1
